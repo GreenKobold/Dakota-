dc27357e0308738291ffb244d2376a26c81c8315
GreenKobold <167018415+GreenKobold@users.noreply.github.com>
2025-10-02 17:37:23 -0400

    Add files via upload

---
+++ Githubnexplanations.cs
@@ -0,0 +1,15 @@
+namespace TestApp21
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+  //Dakota This is my comment hello please comment below with the // command to nullify it as a comment.
+
+        }
+    }
+}
